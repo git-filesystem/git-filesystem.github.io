7fa6feb228a47c8aa04b1f00532ef147bf0b40b8
Toby Smith <tobysmith568@hotmail.co.uk>
2022-10-13 00:58:02 +0100

    Start to add the TypeScript documentation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styled from "@emotion/styled";
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout description="Read & write to GitHub/GitLab/Bitbucket repositories like they're file systems.">
       <Main>
         <div>
           <Header>Git-FileSystem</Header>
